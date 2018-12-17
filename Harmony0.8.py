62037295aacf00c9f905d3cd5b40f8852a61da29
Samuel Butler <sambutler@uchicago.edu>
2018-12-17 15:18:00 -0600

    note can be places but two in column crashes

--- Harmony0.8.py
+++ Harmony0.8.py
@@ -16,6 +16,8 @@ class Interface:
 		self.screen.fill(self.white)
 		pygame.display.flip()
 		self.listOfNotesStaffOne = [(-1) for i in range(10)]
+		self.listOfNotesStaffTwo = [(-1) for i in range(10)]
+
 
 	#def whenThereIsAClick(self):
 
@@ -30,8 +32,8 @@ class Interface:
 		self.horizontalLineNumberStaffTwo = 0
 		self.screenWidth = screenInfo.current_w
 		self.screenHeight = screenInfo.current_h
-		noteStaffOne = NoteInteractionStaffOne()
-		noteStaffTwo = NoteInteractionStaffTwo()
+		# noteStaffOne = NoteInteractionStaffOne()
+		# noteStaffTwo = NoteInteractionStaffTwo()
 
 		self.clef = pygame.image.load("trebleClef.png")
 		self.clef = pygame.transform.scale(self.clef, (int(132 * self.screenWidth/1280), int(176 * self.screenHeight/720)))
@@ -39,21 +41,21 @@ class Interface:
 		self.matrixOfNotesStaffOne = [["empty" for boxes in range(6)] for collumns in range(10)]
 		self.matrixOfNotesStaffTwo = [["empty" for boxes in range(6)] for collumns in range(10)]
 
-		for nextVarticleLine in range(10):
+		for nextVerticleLine in range(10):
 			pygame.draw.line(self.screen, self.black, (4/15 * self.screenWidth + self.verticleLineNumber * 1/15 * self.screenWidth, self.screenHeight * 4/25), (4/15 * self.screenWidth + self.verticleLineNumber * 1/15 * self.screenWidth, self.screenHeight - self.screenHeight/5), 3)
 			self.verticleLineNumber += 1
 			self.verticleBoxLineNumberStaffOne = 0
 			self.verticleBoxLineNumberStaffTwo = 0
 
 			for nextBoxDown in range(6):
-				noteStaffOne.clickBoxStaffOne(self.screen, self.screenWidth, self.screenHeight, self.verticleLineNumber, self.verticleBoxLineNumberStaffOne)
+				self.matrixOfNotesStaffOne[nextVerticleLine][nextBoxDown] = NoteInteractionStaffOne((nextVerticleLine, nextBoxDown), self.screenWidth, self.screenHeight)
+				self.matrixOfNotesStaffOne[nextVerticleLine][nextBoxDown].clickBoxStaffOne(self.screen, self.screenWidth, self.screenHeight, self.verticleLineNumber, self.verticleBoxLineNumberStaffOne)
 				self.verticleBoxLineNumberStaffOne += 1
-				self.matrixOfNotesStaffOne[nextVerticleLine][nextBoxDown] = NoteInteractionStaffOne((nextVerticleLine, nextBoxDown))
 
 			for nextBoxDownStaffTwo in range(6):
-				noteStaffTwo.clickBoxStaffTwo(self.screen, self.screenWidth, self.screenHeight, self.verticleLineNumber, self.verticleBoxLineNumberStaffTwo)
+				self.matrixOfNotesStaffTwo[nextVerticleLine][nextBoxDownStaffTwo] = NoteInteractionStaffTwo((nextVerticleLine, nextBoxDownStaffTwo), self.screenWidth, self.screenHeight)
+				self.matrixOfNotesStaffTwo[nextVerticleLine][nextBoxDownStaffTwo].clickBoxStaffTwo(self.screen, self.screenWidth, self.screenHeight, self.verticleLineNumber, self.verticleBoxLineNumberStaffTwo)
 				self.verticleBoxLineNumberStaffTwo += 1
-				self.matrixOfNotesStaffTwo[nextVerticleLine][nextBoxDown] = NoteInteractionStaffTwo((nextVerticleLine, nextBoxDown))
 
 		for nextLineInStaffOne in range(5):
 			pygame.draw.line(self.screen, self.black, (2/15 * self.screenWidth, 1/5 * self.screenHeight + 1/25 * self.screenHeight * self.horizontalLineNumberStaffOne), (self.screenWidth - 11/90 * self.screenWidth, 1/5 * self.screenHeight + 1/25 * self.screenHeight * self.horizontalLineNumberStaffOne), 3)
@@ -68,22 +70,22 @@ class Interface:
 		pygame.display.flip()
 
 	def	WhenABoxIsClickedInStaffOne(self, clickPositionStaffOne):
-		for currentNote in self.matrixOfNotesStaffOne:
-			if currentNote.boxStaffOne.collidepoint(clickPositionStaffOne):
-				pygame.Surface.blit(self.screen, currentNote.note, (currentNote.boxStaffOne.x, currentNote.boxStaffOne.y))
-				if (listOfNotesStaffOne[currentNote.position[0]] != (-1)):
-					pygame.draw.rect(self.screen, self.white, matrixOfNotesStaffOne[currentNote.position[0]][listOfNotesStaffOne[currentNote.position[0]]])
+		for list in self.matrixOfNotesStaffOne:
+			for currentNote in list:
+				if currentNote.boxStaffOne.collidepoint(clickPositionStaffOne):
+					pygame.Surface.blit(self.screen, currentNote.note, (currentNote.boxStaffOne.x, currentNote.boxStaffOne.y))
+					if (self.listOfNotesStaffOne[currentNote.position[0]] != (-1)):
+						pygame.draw.rect(self.screen, self.white, matrixOfNotesStaffOne[currentNote.position[0]][listOfNotesStaffOne[currentNote.position[0]]])
+					self.listOfNotesStaffOne[currentNote.position[0]] = currentNote.position[1]
 
-				self.listOfNotesStaffOne[currentNote.position[0]] = currentNote.position[1]
-
-	def	WhenABoxIsClickedInStaffTwo(self, clickPositionStaffOne):
-			for currentNote in self.matrixOfNotesStaffTwo:
-				if currentNote.boxStaffTwo.collidepoint(clickPositionStaffTwo):
-					pygame.Surface.blit(self.screen, currentNote.note, (currentNote.boxStaffTwo.x, currentNote.boxStaffTwo.y))
-					if (listOfNotesStaffTwo[currentNote.position[0]] != (-1)):
-						pygame.draw.rect(self.screen, self.white, matrixOfNotesStaffTwo[currentNote.position[0]][listOfNotesStaffTwo[currentNote.position[0]]])
-					self.listOfNotesStaffTwo[currentNote.position[0]] = currentNote.position[1]
-
+	def	WhenABoxIsClickedInStaffTwo(self, clickPositionStaffTwo):
+			for list in self.matrixOfNotesStaffTwo:
+				for currentNote in list:
+					if currentNote.boxStaffTwo.collidepoint(clickPositionStaffTwo):
+						pygame.Surface.blit(self.screen, currentNote.note, (currentNote.boxStaffTwo.x, currentNote.boxStaffTwo.y))
+						if (self.listOfNotesStaffTwo[currentNote.position[0]] != (-1)):
+							pygame.draw.rect(self.screen, self.white, matrixOfNotesStaffTwo[currentNote.position[0]][listOfNotesStaffTwo[currentNote.position[0]]])
+						self.listOfNotesStaffTwo[currentNote.position[0]] = currentNote.position[1]
 
 
 
@@ -104,14 +106,7 @@ while True:
 
 	if event.type == pygame.MOUSEBUTTONUP:
 		positionOfTheMouse = pygame.mouse.get_pos()
-		WhenABoxIsClickedInStaffOne(positionOfTheMouse)
-		WhenABoxIsClickedInStaffTwo(positionOfTheMouse)
+		GUI.WhenABoxIsClickedInStaffOne(positionOfTheMouse)
+		GUI.WhenABoxIsClickedInStaffTwo(positionOfTheMouse)
 
 		pygame.display.flip()
-
-
-		#print(event.button)
-		#if event.button == 1:
-		#	print("right click")
-		#if event.button == 3:
-		#	print("left click")
--- noteClass2.py
+++ noteClass2.py
@@ -9,10 +9,7 @@ class NoteInteractionStaffOne:
 
 	def clickBoxStaffOne(self, screen, screenWidth, screenHeight, verticleLineNumber,verticleBoxLineNumberStaffOne):
 		self.boxStaffOne = pygame.Rect(2 + 2/15 * screenWidth + verticleLineNumber * 1/15 * screenWidth, 3 + 1/5 * screenHeight - 1/25 * screenHeight + 1/25 * screenHeight * verticleBoxLineNumberStaffOne, 1/15 * screenWidth - 3, 1/25 * screenHeight - 3)
-		pygame.draw.rect(screen, self.black, self.boxStaffOne, 0)
-
-
-
+		# pygame.draw.rect(screen, self.black, self.boxStaffOne, 0)
 
 class NoteInteractionStaffTwo:
 	def __init__(self, position,screenWidth,screenHeight):
@@ -22,7 +19,6 @@ class NoteInteractionStaffTwo:
 		self.black = [0,0,0]
 		self.note = pygame.transform.scale(pygame.image.load("note.png"), (int( 1/15 * screenWidth - 3), int(1/25 * screenHeight - 3)))
 
-
 	def clickBoxStaffTwo(self, screen, screenWidth, screenHeight, verticleLineNumber,verticleBoxLineNumberStaffTwo):
 		self.boxStaffTwo = pygame.Rect(2 + 2/15 * screenWidth + verticleLineNumber * 1/15 * screenWidth, 3 + 3/5 * screenHeight - 1/25 * screenHeight + 1/25 * screenHeight * verticleBoxLineNumberStaffTwo, 1/15 * screenWidth - 3, 1/25 * screenHeight - 3)
-		pygame.draw.rect(screen, self.black, self.boxStaffTwo, 0)
+		# pygame.draw.rect(screen, self.black, self.boxStaffTwo, 0)
